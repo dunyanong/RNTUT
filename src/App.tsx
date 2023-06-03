6f608cbd8f2b15ce5d983d10866352cb5908cead
Dun Yan <ong.dy2004@gmail.com>
2023-06-03 21:28:36 +0800

    #18

--- src/App.tsx
+++ src/App.tsx
@@ -6,64 +6,20 @@ import {
   Pressable,
 } from 'react-native';
 import { NavigationContainer, RouteProp } from '@react-navigation/native';
-import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack';
+import { createBottomTabNavigator, BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 
-type RootStackParamList = {
-  Screen_A: undefined;
-  Screen_B: undefined;
-};
+import ScreenA from './ScreenA';
+import ScreenB from './ScreenB';
 
-type ScreenANavigationProp = StackNavigationProp<RootStackParamList, 'Screen_A'>;
-type ScreenAProps = {
-  navigation: ScreenANavigationProp;
-};
-
-type ScreenBNavigationProp = StackNavigationProp<RootStackParamList, 'Screen_B'>;
-type ScreenBProps = {
-  navigation: ScreenBNavigationProp;
-};
-
-const Stack = createStackNavigator<RootStackParamList>();
-
-const ScreenA: React.FC<ScreenAProps> = ({ navigation }) => {
-  const onPressHandler = () => {
-    navigation.navigate('Screen_B');
-  };
-
-  return (
-    <View style={styles.body}>
-      <Text style={styles.text}>Screen A</Text>
-      <Pressable onPress={onPressHandler} style={({ pressed }) => ({ backgroundColor: pressed ? '#ddd' : '#0f0' })}>
-        <Text style={styles.text}>Go to Screen B</Text>
-      </Pressable>
-    </View>
-  );
-};
-
-const ScreenB: React.FC<ScreenBProps> = ({ navigation }) => {
-  const onPressHandler = () => {
-    navigation.goBack();
-  };
-
-  return (
-    <View style={styles.body}>
-      <Text style={styles.text}>Screen B</Text>
-      <Pressable
-        onPress={onPressHandler}
-        style={({ pressed }) => ({ backgroundColor: pressed ? '#ddd' : '#0f0' })} >
-        <Text style={styles.text}>Go Back to Screen A</Text>
-      </Pressable>
-    </View>
-  );
-};
+const Tab = createBottomTabNavigator();
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Screen_A" component={ScreenA} />
-        <Stack.Screen name="Screen_B" component={ScreenB} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Screen_A" component={ScreenA} />
+        <Tab.Screen name="Screen_B" component={ScreenB} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
