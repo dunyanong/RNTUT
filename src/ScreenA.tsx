63528db5a211535b050d2eab71e652e7badacb49
Dun Yan <ong.dy2004@gmail.com>
2023-06-05 16:30:22 +0800

    #19

--- src/ScreenA.tsx
+++ src/ScreenA.tsx
@@ -1,48 +1,43 @@
 import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  Pressable,
-} from 'react-native';
-import { createBottomTabNavigator, BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
+import { DrawerNavigationProp } from '@react-navigation/drawer';
 
-type RootBottomTabParamList = {
-    Screen_A: undefined;
-    Screen_B: undefined;
+type RootDrawerParamList = {
+  TabNavigator: undefined;
+  Screen_B: undefined;
 };
 
-type ScreenANavigationProp = BottomTabNavigationProp<RootBottomTabParamList, 'Screen_A'>;
+type ScreenANavigationProp = DrawerNavigationProp<RootDrawerParamList, 'TabNavigator'>;
 type ScreenAProps = {
   navigation: ScreenANavigationProp;
 };
 
 const ScreenA: React.FC<ScreenAProps> = ({ navigation }) => {
-    const onPressHandler = () => {
-      navigation.navigate('Screen_B');
-    };
-  
-    return (
-      <View style={styles.body}>
-        <Text style={styles.text}>Screen A</Text>
-        <Pressable onPress={onPressHandler} style={({ pressed }) => ({ backgroundColor: pressed ? '#ddd' : '#0f0' })}>
-          <Text style={styles.text}>Go to Screen B</Text>
-        </Pressable>
-      </View>
-    );
+  const onPressHandler = () => {
+    navigation.navigate('Screen_B');
+  };
+
+  return (
+    <View style={styles.body}>
+      <Text style={styles.text}>Screen A</Text>
+      <Pressable onPress={onPressHandler} style={({ pressed }) => ({ backgroundColor: pressed ? '#ddd' : '#0f0' })}>
+        <Text style={styles.text}>Go to Screen B</Text>
+      </Pressable>
+    </View>
+  );
 };
 
 export default ScreenA;
 
 const styles = StyleSheet.create({
-    body: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text: {
-        fontSize: 40,
-        fontWeight: 'bold',
-        margin: 10,
-    }
+  body: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    margin: 10,
+  }
 });
